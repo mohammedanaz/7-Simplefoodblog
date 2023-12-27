5e6aa88fe1cb6de92fce36240c62e9ee4f7265e3
mohammedanaz <mohammedanaz@gmail.com>
2023-12-27 08:23:21 +0530

    completed

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
-document.addEventListener("DOMContentLoaded", function() {
+// var verticalSecMenu = document.getElementsByClassName("vertical-sec-menu");
+// // var verticalSecSearch = document.getElementsByClassName("vertical-sec-search");
 
-var verticalSecMenu = document.getElementsByClassName("vertical-sec-menu");
-// var verticalSecSearch = document.getElementsByClassName("vertical-sec-search");
+// var wsY = window.scrollY;
+// var scrollUp = "20px";
 
-var wsY = window.scrollY;
-var scrollUp = "20px";
+// window.addEventListener("scroll", verticalSecMenuAppear);
 
-function verticalSecMenuAppear(){
-    if(wsY > scrollUp){
-        verticalSecMenu.classList.add("appear");
-        // verticalSecSearch.style.top = "60";
+// function verticalSecMenuAppear(){
+//     if(wsY > scrollUp){
+//         verticalSecMenu.classList.add("appear");
+//         // verticalSecSearch.style.top = "60";
 
+//     }
+//     else{
+//         verticalSecMenu.classList.remove("appear");
+//         // verticalSecSearch.style.top = "30";
+//     }
+
+// }
+
+
+
+
+let verticalsecMenu = document.querySelector(".vertical-sec-menu");
+
+let verticalsecSearch = document.querySelector(".vertical-sec-search");
+
+verticalsecSearch.addEventListener("click",menuAppear());
+
+function menuAppear(){
+    verticalsecMenu.classList.toggle(".appear");
     }
-    else{
-        verticalSecMenu.classList.remove("appear");
-        // verticalSecSearch.style.top = "30";
-    }
-
-}
-
-window.addEventListener("scroll", verticalSecMenuAppear);
-
-});
